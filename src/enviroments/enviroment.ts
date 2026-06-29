2a2762fea1fc1370a31ef9a85435630d231fcf70
KariemHatem <kariemhatem@gmail.com>
2026-06-30 01:11:47 +0300

    feat:New Enviroment Url

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,5 +1,6 @@
 export const enviroment = {
   production: false,
-  BaseURL:
-    "https://376554e4-a589-443a-aee6-696a6a55e252-00-2bfom7xdsl2gz.spock.replit.dev/api",
+  // BaseURL:
+  //   "https://376554e4-a589-443a-aee6-696a6a55e252-00-2bfom7xdsl2gz.spock.replit.dev/api",
+  BaseURL: "https://wttracker-production.up.railway.app/api",
 };
